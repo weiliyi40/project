b647d4bb3e74693e0d6df87b467fa2f8a5fa3a2b
weiliyi40 <906638904@qq.com>
2020-11-16 21:43:31 +0800

    修改 APP.js

---
+++ src/components/app.jsx
@@ -0,0 +1,7 @@
+import { hot } from 'react-hot-loader/root';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";  
+                class App extends React.Component{
+                    
+                }
