81b8c816b08f4d5e3f0f04b214313710b868f768
weiliyi40 <906638904@qq.com>
2020-12-01 20:10:09 +0800

    代码分离:vendor与bundle

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,17 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { request } = require("http");
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = function (env, argv) {
   const isEnvDevelopment = argv.mode === "development" || !argv.mode;
   const isEnvProduction = argv.mode === "production";
   const webpack = require('webpack')
-  
+
   return {
     mode: isEnvProduction ? "production" : isEnvDevelopment && "development",
+    
     devtool: isEnvProduction
       ? "source-map"
       : isEnvDevelopment && "cheap-module-source-map",
@@ -17,10 +20,10 @@ module.exports = function (env, argv) {
       filename: "bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
-    devServer: {
-      contentBase: "./dist",
-      hot: true
-    },
+    // devServer: {
+    //   contentBase: "./dist",
+    //   hot: true
+    // },
     module: {
       rules: [
         {
@@ -33,14 +36,14 @@ module.exports = function (env, argv) {
           test: /\.css$/,
           include:[path.resolve(__dirname,'src/styles'),/node_modules/],
           use: [
-            "style-loader",
+            MiniCssExtractPlugin.loader,
             "css-loader","postcss-loader"
           ]
         },
         {
           test: /\.css$/,
           exclude:[path.resolve(__dirname,'src/styles'),/node_modules/],
-          use:["style-loader","css-loader?modules","postcss-loader"]
+          use:[ MiniCssExtractPlugin.loader,"css-loader?modules","postcss-loader"]
         },
         {
           test: /\.less$/,
@@ -68,13 +71,35 @@ module.exports = function (env, argv) {
 
       ],
     },
+    optimization:{
+      splitChunks:{
+        chunks:'all',
+        name:true,
+        cacheGroups:{
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        },
+      },
 
+    },
     plugins: [
       new HtmlWebpackPlugin({
         template: "public/index.html",
       }),
-      new webpack.NamedChunksPlugin(),
-      new webpack.HotModuleReplacementPlugin()
+    //   new webpack.NamedChunksPlugin(),
+    //   new webpack.HotModuleReplacementPlugin(),
+      new MiniCssExtractPlugin({
+          filename:'[name].[contenthash:8].css',
+          chunkFilename:'[name].[contenthash:8].chunk.css',          
+      }),
+      new BundleAnalyzerPlugin(),
     ],
   };
 };
